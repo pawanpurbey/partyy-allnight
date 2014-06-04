8e4cca773f2bb6c915721cc91b3fe3824682e121
utkarshTripathi <utkarshtripathi1@gmail.com>
2014-06-04 11:03:53 +0530

    Action bar added

--- partyy/src/com/example/partyy/OpenVenueActivity.java
+++ partyy/src/com/example/partyy/OpenVenueActivity.java
@@ -8,8 +8,12 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView.FindListener;
 import android.widget.Button;
@@ -17,7 +21,7 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class OpenVenueActivity extends Activity{
+public class OpenVenueActivity extends ActionBarActivity{
 	private TextView view;
 	protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
@@ -30,7 +34,7 @@ public class OpenVenueActivity extends Activity{
 		    //Now get actual data to show 
 		    VenueData data = DataArray.getInstance().vecVenueData.elementAt(valPos);
 		   
-		    if(data== null || data.btmmap == null){
+		    /*if(data== null || data.btmmap == null){
 		    	//LayoutInflater inflater = getLayoutInflater();
 		    	getWindow().getDecorView().setBackgroundResource(R.drawable.party);
 		        
@@ -46,10 +50,38 @@ public class OpenVenueActivity extends Activity{
     	    	Drawable drawable = new BitmapDrawable(this.getResources(), bitmap);
     	    	getWindow().getDecorView().setBackgroundDrawable(drawable);
     	    }
-		    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?daddr="+28.523056+","+77.2075));
+		    /*Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?daddr="+28.523056+","+77.2075));
 		    intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
 
-            startActivity(intent);
+            startActivity(intent);*/
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.menu, menu);
+	    return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            //openSearch();
+	            return true;
+	        case R.id.action_navigate:
+	            openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	private void openSettings() {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?daddr="+28.523056+","+77.2075));
+	    intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+
+        startActivity(intent);
 	}
  }
 
